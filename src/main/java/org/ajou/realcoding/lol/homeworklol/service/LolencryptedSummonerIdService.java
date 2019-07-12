1d42c6de0686bf1c9b3ec3614ff7c71197a2b078
jinu <jjw8967@ajou.ac.kr>
2019-07-12 14:55:19 +0900

    Initialize MongoDB

--- src/main/java/org/ajou/realcoding/lol/homeworklol/service/LolencryptedSummonerIdService.java
+++ src/main/java/org/ajou/realcoding/lol/homeworklol/service/LolencryptedSummonerIdService.java
@@ -1,8 +1,23 @@
 package org.ajou.realcoding.lol.homeworklol.service;
 
+import org.ajou.realcoding.lol.homeworklol.api.SummonerIdApiClient;
+import org.ajou.realcoding.lol.homeworklol.domain.SummonerDTO;
+import org.ajou.realcoding.lol.homeworklol.repository.SummonerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LolencryptedSummonerIdService {
+    @Autowired
+    private SummonerRepository summonerRepository;
+    @Autowired
+    private SummonerIdApiClient summonerIdApiClient;
 
+
+    public SummonerDTO insertSummonerDTOBySummonerBySummonerName(String name){
+
+        SummonerDTO summonerDTO = summonerIdApiClient.requestSummonerDTO(name);
+
+        return summonerRepository.insertOrUpdatedSummonerDTO(summonerDTO);
+    }
 }
